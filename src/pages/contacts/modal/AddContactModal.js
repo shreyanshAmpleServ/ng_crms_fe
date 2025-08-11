893c474357045bdf035be31d8dae631b9590670e
Rajesh Panwar <rajesh.panwar@DCC.local>
2025-08-11 08:53:56 +0300

    new update

--- src/pages/contacts/modal/AddContactModal.js
+++ src/pages/contacts/modal/AddContactModal.js
@@ -813,6 +813,7 @@ const AddContactModal = ({ contact, setSelectedContact }) => {
                       </div>
                     </div>
                     {/* <div className="col-md-6">
+                    
                       <div className="mb-3">
                         <label className="col-form-label">
                           Industry <span className="text-danger">*</span>
